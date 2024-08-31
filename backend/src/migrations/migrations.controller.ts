0f14963145e6bc584c785fc5d525476282f316e9
Artem Belyavsky <artembell.dev@gmail.com>
2024-09-01 00:00:32 +0300

    add migrations working

--- backend/src/migrations/migrations.controller.ts
+++ backend/src/migrations/migrations.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post } from '@nestjs/common';
 import { MigrationsService } from './migrations.service';
+import { Public } from 'src/decorators/public';
 
 @Controller('migrations')
 export class MigrationsController {
@@ -7,12 +8,14 @@ export class MigrationsController {
         private readonly migrationsService: MigrationsService
     ) { }
 
+    @Public()
     @Post('/init')
     async initMigrations() {
         await this.migrationsService.migrate();
         return {};
     }
 
+    @Public()
     @Post('/reset')
     async resetMigrations() {
         await this.migrationsService.clear();
